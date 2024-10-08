28016d08f924ea43e03a25acf8162149e7849dce
rm6alalauau <rm6alalauau01@gmail.com>
2024-10-08 16:16:22 +0800

    1008-5

--- src/components/PixivCard.vue
+++ src/components/PixivCard.vue
@@ -48,7 +48,7 @@
               target="_blank"
             >
               <v-img
-                :src="`/pixiv-image${item.imageUrl}`"
+                :src="`https://i.pximg.net/${item.imageUrl}`"
                 height="160"
                 width="160"
                 class="d-block mx-auto"
@@ -61,7 +61,7 @@
                   :href="`https://www.pixiv.net/users/${item.userId}`"
                   target="_blank"
                 >
-                  <img :src="`/pixiv-image${item.authorAvatar}`" alt="Avatar" />
+                  <img :src="`https://i.pximg.net/${item.authorAvatar}`" alt="Avatar" />
                 </a>
               </v-avatar>
               <span class="ml-2 text-ellipsis">{{ item.authorName }}</span>
@@ -105,15 +105,15 @@ export default {
         switch (this.selectedOption.value) {
           case "100+":
             url =
-              "https://cloudflare-cors-anywhere.zzz-archive-back-end.workers.dev/?https://www.pixiv.net/ajax/search/artworks/ゼンゼロ100users入り?word=ゼンゼロ100users入り&mode=safe&lang=zh";
+              "https://www.pixiv.net/ajax/search/artworks/ゼンゼロ100users入り?word=ゼンゼロ100users入り&mode=safe&lang=zh";
             break;
           case "500+":
             url =
-              "https://cloudflare-cors-anywhere.zzz-archive-back-end.workers.dev/?https://www.pixiv.net/ajax/search/artworks/ゼンゼロ500users入り?word=ゼンゼロ500users入り&mode=safe&lang=zh";
+              "https://www.pixiv.net/ajax/search/artworks/ゼンゼロ500users入り?word=ゼンゼロ500users入り&mode=safe&lang=zh";
             break;
           case "1000+":
             url =
-              "https://cloudflare-cors-anywhere.zzz-archive-back-end.workers.dev/?https://www.pixiv.net/ajax/search/artworks/ゼンゼロ1000users入り?word=ゼンゼロ1000users入り&mode=safe&lang=zh";
+              "https://www.pixiv.net/ajax/search/artworks/ゼンゼロ1000users入り?word=ゼンゼロ1000users入り&mode=safe&lang=zh";
             break;
         }
         console.log("Fetching URL:", url);
